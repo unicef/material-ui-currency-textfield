bbad00bbe93836acd9737000a868d9daf215a909
Tarik Caramanico <tarikcaramanico@gmail.com>
2020-08-18 12:06:47 -0300

    Fix example reference

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import CurrencyTextField from "./CurrencyTextField"
+import CurrencyTextField from "@unicef/material-ui-currency-textfield"
 import { Button } from "@material-ui/core"
 
 export default function App() {

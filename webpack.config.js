4bbe559ae26c0f1bb10066af8b9f817f26844caa
Emre Cilesiz <cilesizemre@outlook.de>
2022-12-23 01:13:12 +0100

    Also parse jsx files.

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,14 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-        ],
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     },
     externals: {
         react: "react",
